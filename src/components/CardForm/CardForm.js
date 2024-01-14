6a1e1284074a62dbef60332fd36b5424d56d01ef
jerzy szajner <jerzy.szajner@gmail.com>
2024-01-14 14:29:20 +0100

    feat(redux): Refactor State Management with useSelector and Redux Actions
    - Integrate useSelector hook in List and Column components for accessing Redux state, focusing on the selection of columns and cards based on IDs.
    - Implement Redux actions 'ADD_COLUMN' and 'ADD_CARD' with unique IDs generated using shortid, enhancing the state management process.
    - Revamp the CardForm component to dispatch 'ADD_CARD' action upon form submission, streamlining card creation.
    - Simplify the Column component's structure by retrieving relevant cards directly from the Redux store using useSelector, eliminating prop drilling.
    - Enhance the overall state structure and data flow by incorporating Redux for centralized state management and easier maintenance.
    
    This commit significantly improves the application's data handling and component structure by employing Redux and useSelector for more efficient and maintainable state management.

--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -2,19 +2,21 @@ import styles from './CardForm.module.scss';
 import { useState } from 'react';
 import Button from './../Button/Button';
 import TextInput from './../TextInput/TextInput';
+import { useDispatch } from 'react-redux';
 
 const CardForm = props => {
     const [title, setTitle] = useState('');
+    const dispatch = useDispatch();
 
     const handleSubmit = e => {
         e.preventDefault();
-        props.action({ title: title }, props.columnId);
+        dispatch({ type: 'ADD_CARD', payload: { title, columnId: props.columnId } });
         setTitle('');
     };
 
     return (
         <form className={styles.cardForm} onSubmit={handleSubmit}>
-            <label className={styles.label}></label>
+            <label className={styles.label}>Title:</label>
             <TextInput value={title} onChange={e => setTitle(e.target.value)} />
             <Button>Add card</Button>
         </form>
--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -2,14 +2,16 @@ import styles from './ColumnForm.module.scss';
 import { useState } from 'react';
 import Button from '../Button/Button';
 import TextInput from '../TextInput/TextInput';
+import { useDispatch } from 'react-redux';
 
 const ColumnForm = props => {
     const [title, setTitle] = useState('');
     const [icon, setIcon] = useState('');
+    const dispatch = useDispatch();
 
     const handleSubmit = e => {
         e.preventDefault();
-        props.action({ title: title, icon: icon });
+        dispatch({ type: 'ADD_COLUMN', payload: { title, icon } });
         setTitle('');
         setIcon('');
     };
@@ -18,10 +20,10 @@ const ColumnForm = props => {
         <form onSubmit={handleSubmit} className={styles.columnForm}>
             <label className={styles.label}>Title:</label>
             <TextInput value={title} onChange={e => setTitle(e.target.value)} />
-    
+
             <label className={styles.label}>Icon:</label>
             <TextInput value={icon} onChange={e => setIcon(e.target.value)} />
-    
+
             <Button>Add column</Button>
         </form>
     );
